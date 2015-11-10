2e7fac861cb19162668709663764fff8891088f8
yangyang <451806617@qq.com>
2015-11-10 18:09:05 +0800

    阴影

--- 点击按钮阴影出现，点击阴影，阴影退出/ViewController.m
+++ 点击按钮阴影出现，点击阴影，阴影退出/ViewController.m
@@ -17,8 +17,28 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    UIButton *myButton =[[UIButton alloc]initWithFrame:CGRectMake(50, 50, 100, 100)];
+    myButton.backgroundColor =[UIColor redColor];
+    [myButton setTitle:@"开始" forState:UIControlStateNormal];
+    [myButton addTarget:self action:@selector(Clickbutton) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:myButton];
 }
-
+-(void)Clickbutton{
+   // NSLog(@"sfdsgdg");
+    _shadow =[[UIView alloc]initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
+    [_shadow addGestureRecognizer:[[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(Clickshadow)]];
+    _shadow.alpha =0.1;
+    _shadow.backgroundColor =[UIColor blackColor];
+    [self.view addSubview:_shadow];
+    [UIView animateWithDuration:0.3 animations:^{
+    _shadow.alpha =0.3;
+    }];
+    
+}
+-(void)Clickshadow{
+    _shadow.alpha =0.1;
+    [_shadow removeFromSuperview];
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
